7ccfafbfcbb479151062d60a42050c027e802ee7
rizemun <rizeman01@gmail.com>
2019-06-21 00:15:37 +0300

    fix port selection

--- index.js
+++ index.js
@@ -196,10 +196,8 @@ app.ws('/', function(ws, req){
 
 //Start server
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-    port = 3000;
-}
+let port = process.env.PORT || 3000;
+
 app.listen(port, function () {
     console.log('Server started on port 3000');
 });
