5dfd5dea43b2d25f4fe0b068bc34bfbfbcd436de
rizemun <rizeman01@gmail.com>
2019-06-21 00:04:14 +0300

    start setting heroku deploy

--- index.js
+++ index.js
@@ -195,6 +195,11 @@ app.ws('/', function(ws, req){
 
 
 //Start server
-app.listen(3000, function () {
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+app.listen(port, function () {
     console.log('Server started on port 3000');
 });
